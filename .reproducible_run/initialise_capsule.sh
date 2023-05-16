8d6e385abfe26b84ee71ff4e7a87203937d567b9
Sofian Hamiti <sofian.hamiti@gmail.com>
2023-05-16 11:41:55 +0100

    Update initialise_capsule.sh

--- .reproducible_run/initialise_capsule.sh
+++ .reproducible_run/initialise_capsule.sh
@@ -8,6 +8,7 @@ pip install -r requirements.txt
 ##################
 dvc init
 dvc config core.analytics false
+dvc config core.autostage true
 
 ##################
 # CREATE DATA AND RESULTS DIRECTORY
