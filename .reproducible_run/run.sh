fb352abc4685630edc72254a89ff6db8b959b3d4
Sofian Hamiti <sofian.hamiti@gmail.com>
2023-05-16 11:48:02 +0100

    Update run.sh

--- .reproducible_run/run.sh
+++ .reproducible_run/run.sh
@@ -1,30 +1,40 @@
 #!/usr/bin/env bash
 
 ##################
-# GET CAPSULE NAME AND GENERATE CAPSULE VERSION
+# GET ANALYSIS NAME AND GENERATE ANALYSIS VERSION
 ##################
 # Read the YAML file
 yaml=$(cat environment/config.yaml)
 
-CAPSULE_NAME=${PWD##*/} # get name from parent directory
-CAPSULE_BUCKET=$(echo "$yaml" | sed -n 's/^ *s3_bucket: *//p')
-CAPSULE_VERSION=$(uuidgen | cut -c-8)
+ANALYSIS_NAME=${PWD##*/} # get name from parent directory
+ANALYSIS_VERSION=$(uuidgen | cut -c-8)
+AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
+AWS_REGION=$(aws configure get region)
+ANALYSIS_BUCKET="s3://sagemaker-${AWS_REGION}-${AWS_ACCOUNT}"
 
 #################
-VERSIONING DATASET
+# VERSIONING DATASET
 #################
 echo "===== VERSIONING DATASET ====="
-dvc remote remove s3bucket
-dvc remote add -d s3bucket "${CAPSULE_BUCKET}/${CAPSULE_NAME}"
+# Check if DVC remote exists
+if dvc remote list | grep 's3bucket' >/dev/null 2>&1; then
+    echo "DVC config setup. Nothing to add"
+    dvc remote remove s3bucket
+    dvc remote add -d s3bucket "${ANALYSIS_BUCKET}/${ANALYSIS_NAME}"
+else
+    echo "DVC config not setup. Adding..."
+    dvc remote add -d s3bucket "${ANALYSIS_BUCKET}/${ANALYSIS_NAME}"
+fi
+
 dvc add data/
 dvc push
 
 ##################
-# SAVE CAPSULE VERSION TO GITHUB REPO
+# SAVE ANALYSIS VERSION TO GITHUB REPO
 ##################
-echo "===== SAVING CAPSULE INTO GITHUB ====="
+echo "===== SAVING ANALYSIS INTO GITHUB ====="
 git add .
-git commit -m ${CAPSULE_VERSION}
+git commit -m ${ANALYSIS_VERSION}
 git push
 
 ##################
@@ -35,22 +45,13 @@ git push
 ##################
 # BUILD AND PUSH CONTAINER TO ECR
 ##################
-# Get the account number associated with the current IAM credentials
-AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
-if [[ $? -ne 0 ]]
-then
-    exit 25
-fi
-
-# Get the region defined in the current configuration (default to us-west-2 if none defined)
-AWS_REGION=$(aws configure get region)
-CONTAINER_FULLNAME="${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${CAPSULE_NAME}:${CAPSULE_VERSION}"
+CONTAINER_FULLNAME="${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ANALYSIS_NAME}:${ANALYSIS_VERSION}"
 
 # If the repository doesn't exist in ECR, create it.
-aws ecr describe-repositories --repository-names "${CAPSULE_NAME}" > /dev/null 2>&1
+aws ecr describe-repositories --repository-names "${ANALYSIS_NAME}" > /dev/null 2>&1
 if [[ $? -ne 0 ]]
 then
-    aws ecr create-repository --repository-name "${CAPSULE_NAME}" > /dev/null
+    aws ecr create-repository --repository-name "${ANALYSIS_NAME}" > /dev/null
 fi
 
 # Get the login command from ECR and execute it directly
@@ -58,8 +59,8 @@ $(aws ecr get-login --region ${AWS_REGION} --no-include-email)
 
 # Build the docker image locally with the image name and then push it to ECR
 echo "===== BUILDING CONTAINER IMAGE ====="
-docker build --no-cache -t ${CAPSULE_NAME} -f environment/Dockerfile .
-docker tag ${CAPSULE_NAME} ${CONTAINER_FULLNAME}
+docker build --no-cache -t ${ANALYSIS_NAME} -f environment/Dockerfile .
+docker tag ${ANALYSIS_NAME} ${CONTAINER_FULLNAME}
 
 echo "===== PUSHING CONTAINER IMAGE TO ECR ====="
 docker push ${CONTAINER_FULLNAME}
@@ -67,10 +68,10 @@ docker push ${CONTAINER_FULLNAME}
 ##################
 # LAUNCH JOB IN SAGEMAKER
 ##################
-echo "===== RUNNING THE CAPSULE ON AWS ====="
+echo "===== RUNNING THE ANALYSIS ON AWS ====="
 python .reproducible_run/job.py \
-    --job_name ${CAPSULE_NAME}-${CAPSULE_VERSION} \
+    --job_name ${ANALYSIS_NAME}-${ANALYSIS_VERSION} \
     --container_image ${CONTAINER_FULLNAME} \
     --entrypoint code/entrypoint.sh
 
-echo "===== CAPSULE EXECUTED SUCCESSFULLY ====="
+echo "===== ANALYSIS EXECUTED SUCCESSFULLY ====="
